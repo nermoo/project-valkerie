1778e9f13892b9e62bce2749a06ad8bc1514490b
nermoo <nawarathnaaravinda@gmail.com>
2024-03-09 20:42:19 +0530

    Added less config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,12 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "project": "./tsconfig.json",
+    "extraFileExtensions": [".json",".less", ".css"]
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
---
+++ src/components/Counter/counter.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import styles from './counter.module.less'
+
+const counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className={styles.counterCrd}>
+    <button onClick={() => setCount((count) => count + 1)}>
+      count {count}
+    </button>
+  </div>
+  )
+}
+
+export default counter;
