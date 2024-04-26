f72ff412cced7ddef1433507185509947d3fff30
nermoo <nawarathnaaravinda@gmail.com>
2024-04-26 09:54:04 +0530

    update workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Counter from './components/Counter/counter';
-import styles from './App.module.less'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from './pages/Homepage/homepage';
+import Login from './pages/Login/login';
+
 
 function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className={styles.logo} alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={styles.card}>
-        <Counter/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className={styles.read_the_docs}>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+// import {
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//   RouterProvider,
+// } from "react-router-dom";
+// import Counter from './components/Counter/counter';
+
 import './index.module.less'
+
+
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="project-valkerie/" element={<Counter />}>
+//       <Route path="counter" element={<App />} />
+//     </Route>
+//   )
+// );
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
 )
